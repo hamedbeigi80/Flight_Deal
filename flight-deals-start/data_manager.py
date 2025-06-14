c046d9871bbf4f3469bbdb0a610f7f5601acff73
hamedbeigi80 <hamedahmadbeigi@gmail.com>
2025-06-14 19:08:45 +0330

    Update data_manager.py

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,37 +1,34 @@
 import os
 import requests
 from requests.auth import HTTPBasicAuth
-from  dotenv import load_dotenv
+from dotenv import load_dotenv
+
 # Load environment variables from .env file
 load_dotenv()
 
 SHEETY_PRICES_ENDPOINT = os.environ["SHEETY_PRICES_ENDPOINT"]
 SHEETY_USERS_ENDPOINT = os.environ["SHEETY_USERS_ENDPOINT"]
 
-class DataManager:
-    # This class is responsible for talking to the Google Sheet.
-    def __init__(self):
 
-        self._user = os.environ["SHEETY_USRERNAME"]
+class DataManager:
+    """This class is responsible for talking to the Google Sheet via Sheety API."""
+    
+    def __init__(self):
+        self._user = os.environ["SHEETY_USERNAME"]
         self._password = os.environ["SHEETY_PASSWORD"]
         self._authorization = HTTPBasicAuth(self._user, self._password)
         self.destination_data = {}
-        self._user_emails = []
-        self.customer_data={}
-
+        self.customer_data = {}
 
     def get_destination_data(self):
-        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        """Get all destination data from the Google Sheet."""
         response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
         data = response.json()
         self.destination_data = data["prices"]
-        # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
-        # pprint(data)
         return self.destination_data
 
-        # 6. In the DataManager Class make a PUT request and use the row id from sheet_data
-        # to update the Google Sheet with the IATA codes. (Do this using code).
     def update_destination_codes(self):
+        """Update the Google Sheet with IATA codes for each destination."""
         for city in self.destination_data:
             new_data = {
                 "price": {
@@ -44,12 +41,10 @@ class DataManager:
                 auth=self._authorization
             )
             print(response.text)
+
     def get_customer_emails(self):
-        response = requests.get(url=SHEETY_USERS_ENDPOINT)
+        """Get customer email list from the Google Sheet."""
+        response = requests.get(url=SHEETY_USERS_ENDPOINT, auth=self._authorization)
         data = response.json()
-        # See how Sheet data is formatted so that you use the correct column name!
-        # pprint(data)
-        # Name of spreadsheet 'tab' with the customer emails should be "users".
         self.customer_data = data["users"]
         return self.customer_data
-
